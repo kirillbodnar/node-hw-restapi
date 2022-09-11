8a50c12b3882ab8378174e421777ee84a9921501
kirillbodnar <91017699+kirillbodnar@users.noreply.github.com>
2022-09-12 01:01:06 +0300

    hw3 fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
 
@@ -11,16 +12,15 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, _, res, __) => {
-  const { status = 500, message = "Server error" } = err;
-
-  res.status(status).json({ message });
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
 });
 
 module.exports = app;
