5023a048d04b4c76291c1b80637d59ca7eb4a1d3
kirillbodnar <91017699+kirillbodnar@users.noreply.github.com>
2022-09-18 04:40:43 +0300

    hw 5 done

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const isValidId = require("./isValidId");
 const validationBody = require("./validationBody");
 const authentication = require("./authentication");
+const upload = require("./uplaod");
 
 module.exports = {
   isValidId,
   validationBody,
   authentication,
+  upload,
 };
